56fb6c8c6b6201902791b3804e3e423c5749840d
xspin <xspin@github.com>
2019-10-09 13:10:18 +0800

    pkill

---
+++ pkill.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+kill_all() {
+    for pid in `printf "$PSDATA" | awk '{print $2}'`
+    do
+        echo "kill Pid $pid"
+        kill $pid && kill -9 $pid
+    done
+}
+
+if [ -z "$1" ]; then echo "Usage: kill.sh [Program_Name]"; exit; fi
+
+PSDATA=`ps aux | grep "$1" | grep -v grep | grep -v kill`
+
+if [ ! -z "$PSDATA" ]; then
+    echo `ps aux|head -n1`
+    printf "$PSDATA" | grep --color=auto "$1" 
+fi
+echo '------------------------------------------'
+
+if  [ -z "$PSDATA" ]; then
+    echo 'No progress found.'
+    exit
+fi
+
+read -p "Do you want to kill all processes (y/n)? " yn
+case $yn in
+    [Yy]* ) kill_all; exit;;
+    [Nn]* ) exit;;
+    * ) echo "Please answer yes or no.";;
+esac
