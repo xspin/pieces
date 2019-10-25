870152e82f6bd1b3c714ac80e42486a60dc37df1
xspin <xspin@github.com>
2019-10-25 17:36:21 +0800

    update

--- jupyter.sh
+++ jupyter.sh
@@ -4,25 +4,25 @@ PIP=pip
 # jupyter_dir=$(jupyter --data-dir)
 jupyter_dir='/usr/local/share/jupyter'
 
-echo -e "\033[35mInstall nbextensions ...\033[0m"
+echo -e "\033[31mInstall nbextensions ...\033[0m"
 $PIP install jupyter_contrib_nbextensions
 jupyter contrib nbextension install
 
-echo -e "\033[35mInstall jupyterthemes ...\033[0m"
+echo -e "\033[31mInstall jupyterthemes ...\033[0m"
 $PIP install jupyterthemes
 jt -t onedork -f roboto -fs 10 -nfs 9 -vim -T
 
-echo -e "\033[35mInstall vim_binding ...\033[0m"
+echo -e "\033[31mInstall vim_binding ...\033[0m"
 cd $jupyter_dir/nbextensions
 git clone https://github.com/lambdalisue/jupyter-vim-binding vim_binding
 
-echo -e "\033[35mEnable extensions ...\033[0m"
+echo -e "\033[31mEnable extensions ...\033[0m"
 jupyter nbextension enable vim_binding/vim_binding
 jupyter nbextension enable execute_time/ExecuteTime
 jupyter nbextension enable codefolding/main
 jupyter nbextension enable toggle_all_line_numbers/main
 #jupyter nbextension enable code_prettify/code_prettify
 
-echo -e "\033[35mRestart jupyter ...\033[0m"
+echo -e "\033[31mRestart jupyter ...\033[0m"
 pid=`ps x|grep jupyter-notebook|grep -v grep|awk '{print $1}'`
 nohup bash -c "kill $pid; /usr/bin/start_jupyter.sh" > /tmp/jupyter.log &
